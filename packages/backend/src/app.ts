389b8da71903f7295ccacf7ab399ad912075f6c0
Alexander Cai <alexandercai@outlook.com>
2020-02-22 08:03:31 -0700

    add errorhandler middleware and basic logging

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,21 +1,26 @@
 import express from 'express';
+import errorHandler from 'errorhandler';
 import cors from 'cors';
-import func from './asdf';
+import logger from './util/logger';
 
 const app = express();
-const port = 3001;
-app.use(cors());
 
-// _req underscore so that we tell the linter that we
-// intentionally aren't going to use it. Can also do
-// `{}` instead of `_req`
+app.set('port', process.env.PORT || 3001);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (_req, res) => {
-  // eslint-disable-next-line no-console
-  console.log('Hello123123');
-  func();
   res.send('Hello World!12345 asdf');
 });
 
-app.listen(port);
+app.use(errorHandler());
 
-/* , () => console.log(`Example app listening on port ${port}!`) */
+app.listen(app.get('port'), () => {
+  logger.info(
+    'ProAvalon is running at http://localhost:%d in %s mode',
+    app.get('port'),
+    app.get('env'),
+  );
+  logger.info('Press CTRL-C to stop\n');
+});
